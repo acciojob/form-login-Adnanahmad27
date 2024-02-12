40ef6c0d0e644c115db2682bd4734416cfdd1428
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-13 04:32:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(e) {
+    
+    e.preventDefault(); // Prevent default form submission
 
+    // Access input values
+    var firstName = document.getElementById('fname').value;
+    var lastName = document.getElementById('lname').value;
+
+    // Perform action with input values (e.g., display them)
+    alert(`${firstName} ${lastName}`);
 }
+
+
